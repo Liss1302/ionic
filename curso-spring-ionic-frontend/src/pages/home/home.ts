79773299ec3f618369d0c8e5997087e07b266f01
DSN5131112102\Aluno <liscristinesilva@gmail.com>
2020-10-29 11:18:10 -0200

    Alteracao HomePage

--- curso-spring-ionic-frontend/src/pages/home/home.ts
+++ curso-spring-ionic-frontend/src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { NavController,IonicPage } from 'ionic-angular';
+import { NavController,IonicPage, MenuController } from 'ionic-angular';
 
 @IonicPage()
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {} 
-  login(){
-    this.navCtrl.setRoot("CategoriasPage");
+
+  constructor(public navCtrl: NavController, public menu: MenuController) {
+
+  }
+  loging(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+  ionViewDidLeave(){
+    this.menu.swipeEnable(true);
+
   }
 }
